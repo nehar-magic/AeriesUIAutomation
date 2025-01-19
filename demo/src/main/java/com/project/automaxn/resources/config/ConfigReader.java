ffc28d2830b638d5952696e19650b7b053eb6fe2
nehar.magic <neha.rajput@magicedtech.com>
2025-01-19 16:15:38 +0530

    Updated config readers' file paths to be dynamic

--- demo/src/main/java/com/project/automaxn/resources/config/ConfigReader.java
+++ demo/src/main/java/com/project/automaxn/resources/config/ConfigReader.java
@@ -5,19 +5,20 @@ import java.io.IOException;
 import java.util.Properties;
 
 public class ConfigReader {
+
     private static Properties properties = new Properties();
 
     static {
-    try {
-        System.out.println("Loading configuration file...");
-        FileInputStream file = new FileInputStream("D:\\Files\\SeleniumUIAutomationProject\\demo\\src\\main\\java\\com\\project\\automaxn\\resources\\config\\config.properties");
-        properties.load(file);
-        System.out.println("Configuration file loaded successfully.");
-    } catch (IOException e) {
-        e.printStackTrace();
-        throw new RuntimeException("Failed to load configuration file: " + e.getMessage());
+        try {
+            System.out.println("Loading configuration file...");
+            FileInputStream file = new FileInputStream("src/main/java/com/project/automaxn/resources/config/config.properties");
+            properties.load(file);
+            System.out.println("Configuration file loaded successfully.");
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Failed to load configuration file: " + e.getMessage());
+        }
     }
-}
 
     public static String getProperty(String key) {
         String value = properties.getProperty(key);
--- demo/src/main/java/com/project/automaxn/utils/JsonConfigReader.java
+++ demo/src/main/java/com/project/automaxn/utils/JsonConfigReader.java
@@ -14,7 +14,8 @@ public class JsonConfigReader {
 
     // Static block to load the JSON file at runtime
     static {
-        try (InputStream inputStream = Files.newInputStream(Paths.get("D:\\Files\\SeleniumUIAutomationProject\\demo\\src\\main\\java\\com\\project\\automaxn\\resources\\config\\credentials.json"))) {
+        try (InputStream inputStream = Files.newInputStream(
+                Paths.get("src/main/java/com/project/automaxn/resources/config/credentials.json"))) {
             Gson gson = new Gson();
             credentials = gson.fromJson(new InputStreamReader(inputStream), Map.class);
         } catch (Exception e) {
